7581260a2d1649ac3091a7378d19b5914019d40b
michheusser <services.michel@icloud.com>
2025-10-12 00:02:20 +0200

    Passing of context tuple into edge/node tuple

--- source/core/include/weave/core/Graph.h
+++ source/core/include/weave/core/Graph.h
@@ -15,8 +15,11 @@ namespace weave
 		class Graph
 		{
 		public:
-			Graph(typename GraphDescriptorToContextTuples<GraphDescriptorType>::NodeContextTuple& nodeContexts,
-			      typename GraphDescriptorToContextTuples<GraphDescriptorType>::EdgeContextTuple& edgeContexts)
+			using NodesTuple = typename GraphDescriptorToGraphTuples<GraphDescriptorType>::NodesTuple;
+			using EdgesTuple = typename GraphDescriptorToGraphTuples<GraphDescriptorType>::EdgesTuple;
+
+			Graph(NodesTuple& nodeContexts, EdgesTuple& edgeContexts) : _nodes(constructNodes(nodeContexts, std::make_index_sequence<std::tuple_size_v<NodesTuple> >{})),
+			                                                            _edges(constructEdges(edgeContexts, std::make_index_sequence<std::tuple_size_v<EdgesTuple> >{}))
 			{}
 			void initialize()
 			{
@@ -28,8 +31,20 @@ namespace weave
 			}
 
 		private:
-			typename GraphDescriptorToGraphTuples<GraphDescriptorType>::NodesTuple _nodes;
-			typename GraphDescriptorToGraphTuples<GraphDescriptorType>::EdgesTuple _edges;
+			template<typename ContextTuple, std::size_t... Is>
+			static NodesTuple constructNodes(ContextTuple& contexts, std::index_sequence<Is...>)
+			{
+				NodesTuple nodesTuple = {std::tuple_element_t<Is, NodesTuple>(std::get<Is>(contexts))...};
+				return nodesTuple;
+			}
+			template<typename ContextTuple, std::size_t... Is>
+			static EdgesTuple constructEdges(ContextTuple& contexts, std::index_sequence<Is...>)
+			{
+				EdgesTuple edgesTuple = {std::tuple_element_t<Is, EdgesTuple>(std::get<Is>(contexts))...};
+				return edgesTuple;
+			}
+			NodesTuple _nodes;
+			EdgesTuple _edges;
 		};
 	}
 }
